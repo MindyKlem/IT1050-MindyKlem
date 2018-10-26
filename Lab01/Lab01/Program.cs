2ee1c8198c3c01057634ebed8bcd7ece83eb4724
Mindy Klem <S00662626@acad.tri-c.edu>
2018-10-26 16:35:18 -0400

    Created Lab 1

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Mindy Klem");
+            Console.Write("Press any key to cotinue...");
+            Console.ReadKey(true);
+        }
+    }
+}
